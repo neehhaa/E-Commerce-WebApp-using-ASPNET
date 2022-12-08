9d330bd8e7dee81c3190123a5ebab022cbbd765d
neehhaa <nehawasnik95@outlook.com>
2022-12-08 19:06:10 +0530

    models

---
+++ etickets/Models/Actor.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace etickets.Models
+{
+    public class Actor
+    {
+        [Key]
+        public string Id { get; set; }
+        public string ProfilePictureURL { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+    }
+}
---
+++ etickets/Models/Cinema.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace etickets.Models
+{
+    public class Cinema
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Logo { get; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        
+    }
+}
