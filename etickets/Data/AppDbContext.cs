c2fdd8d424613357f5aad70a365caa6ffe8f651e
neehhaa <nehawasnik95@outlook.com>
2022-12-14 01:54:21 +0530

    Migrations

--- etickets/Data/AppDbContext.cs
+++ etickets/Data/AppDbContext.cs
@@ -1,11 +1,10 @@
-﻿using etickets.Models;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
-namespace etickets.Data
+namespace etickets.Models
 {
-    public class AppDbContext:DbContext
+    public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
         }
@@ -16,15 +15,17 @@ namespace etickets.Data
                 am.ActorId,
                 am.MovieId
             });
+
             modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Movie).WithMany(am => am.Actors_Movies).HasForeignKey(m => m.MovieId);
             modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Actor).WithMany(am => am.Actors_Movies).HasForeignKey(m => m.ActorId);
 
-            base.OnModelCreating(modelBuilder);       
+            base.OnModelCreating(modelBuilder);
         }
+
         public DbSet<Actor> Actors { get; set; }
         public DbSet<Movie> Movies { get; set; }
-        public DbSet<Actor_Movie> Actors_Movies { get;set; }
-        public DbSet<Cinema> Cinemas { get; set; } 
+        public DbSet<Actor_Movie> Actors_Movies { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
         public DbSet<Producer> Producers { get; set; }
     }
 }
